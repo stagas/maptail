18a43f8938ee4b01873d067c129f05abb380af03
George Stagas <gstagas@gmail.com>
2011-03-03 18:55:48 +0200

    Working

--- maptail.js
+++ maptail.js
@@ -51,10 +51,8 @@ app.get('/', function(req, res) {
   res.redirect('/map');
 })
 
-// remove the allow() middleware to allow everyone in
 app.get('/map', function(req, res) {
-  //allowedIPs[req.headers.ip] = req.session.user
-  res.render('map', {layout: false, locals: {title: 'tail -f ' + filename } })
+  res.render('map', { layout: false, locals: { title: 'tail -f ' + filename, wshost: wshost, wsport: wsport } })
 })
 
 // get configuration information
@@ -85,8 +83,6 @@ var socket = io.listen(wsserver)
 socket.on('connection', function(client) {
   var id = client.sessionId
     , ip = client && client.request && client.request.socket && client.request.socket.remoteAddress || '000'
-
-  if (typeof allowedIPs[ip] === 'undefined') return
 
   connected[id] = client
 
@@ -197,7 +193,7 @@ tail.stdout.on('data', function (data) {
         if (c === 3) return
       }
       
-      ips[ip] = { name: ip, line: helpful.htmlspecialchars(line), lastActivity: Date.now() }
+      ips[ip] = { name: ip, line: htmlspecialchars(line), lastActivity: Date.now() }
     })
 
     for (var ip in ips) {
@@ -256,4 +252,69 @@ var broadcast = function(msg) {
   for (var id in connected) {
     connected[id].send(json)
   }
+}
+
+var htmlspecialchars = function (string, quote_style, charset, double_encode) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Mirek Slugen
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: Nathan
+  // +   bugfixed by: Arno
+  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: Ratheous
+  // +      input by: Mailfaker (http://www.weedem.fr/)
+  // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: felix
+  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // %        note 1: charset argument not supported
+  // *     example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
+  // *     returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
+  // *     example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
+  // *     returns 2: 'ab"c&#039;d'
+  // *     example 3: htmlspecialchars("my "&entity;" is still here", null, null, false);
+  // *     returns 3: 'my &quot;&entity;&quot; is still here'
+
+  var optTemp = 0, i = 0, noquotes= false;
+  if (typeof quote_style === 'undefined' || quote_style === null) {
+      quote_style = 2;
+  }
+  string = string.toString();
+  if (double_encode !== false) { // Put this first to avoid double-encoding
+      string = string.replace(/&/g, '&amp;');
+  }
+  string = string.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+  var OPTS = {
+      'ENT_NOQUOTES': 0,
+      'ENT_HTML_QUOTE_SINGLE' : 1,
+      'ENT_HTML_QUOTE_DOUBLE' : 2,
+      'ENT_COMPAT': 2,
+      'ENT_QUOTES': 3,
+      'ENT_IGNORE' : 4
+  };
+  if (quote_style === 0) {
+      noquotes = true;
+  }
+  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+      quote_style = [].concat(quote_style);
+      for (i=0; i < quote_style.length; i++) {
+          // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+          if (OPTS[quote_style[i]] === 0) {
+              noquotes = true;
+          }
+          else if (OPTS[quote_style[i]]) {
+              optTemp = optTemp | OPTS[quote_style[i]];
+          }
+      }
+      quote_style = optTemp;
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+      string = string.replace(/'/g, '&#039;');
+  }
+  if (!noquotes) {
+      string = string.replace(/"/g, '&quot;');
+  }
+
+  return string;
 }
