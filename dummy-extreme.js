8772618f28c02b8f9488f009d29ef51f011561c5
SumitJain01 <59796850+SumitJain01@users.noreply.github.com>
2020-10-07 14:54:44 +0530

    Update dummy-extreme.js

--- dummy-extreme.js
+++ dummy-extreme.js
@@ -3,11 +3,11 @@ var colors = require('colors')
 var file = fs.createWriteStream('dummy-log')
 
 function rand () {
-  return Math.floor(Math.random() * 256)
+  return Math.floor(Math.random() * 155)
 }
 
 var i = 0
 setInterval(function () {
-  if (Math.random() * 10 < 5) file.write((i++) + 'gibb'.red + 'er'.yellow + 'ish '.green + [0,0,0,0].map(rand).join('.') + ' more'.cyan + ' gibb'.magenta + 'erish'.blue + '\r\n')
+  if (Math.random() * 10 < 6) file.write((i++) + 'gibb'.red + 'er'.yellow + 'ish '.green + [0,0,0,0].map(rand).join('.') + ' more'.cyan + ' gibb'.magenta + 'erish'.blue + '\r\n')
   else if (Math.random() * 10 > 9) file.write((i++) + ' more gibberish with no ip\n')
 }, 10)
